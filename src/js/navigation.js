59db024f4e50b3edbe63d662d38fccbb54f58c3d
bvbradle@asu.edu <bvbradle@asu.edu>
2024-04-18 17:53:33 -0700

    Changes

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,15 +1,14 @@
 function switchPage(pageNum) {
     const pages = document.querySelectorAll('.page');
     pages.forEach(page => {
-        page.style.display = 'none';  // Hide all pages
+        page.style.display = 'none';
     });
 
     const activePage = document.getElementById('page' + pageNum);
-    activePage.style.display = 'block';  // Show the selected page
+    activePage.style.display = 'block';
 }
 function startGame(gameId) {
     alert("Starting Game " + gameId);
-    // Additional logic to start the game can be added here
 }
 function showGameDetails(gameId) {
     var gameInfo = {
@@ -30,18 +29,35 @@ function hideGameDetails() {
     document.getElementById('gameDetails').style.display = 'none';
 }
 
-function filterCourses() {
-    var input, filter, ul, li, i, txtValue;
-    input = document.getElementById("courseSearchInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("courseList");
-    course = div.getElementsByClassName("course-item");
-    for (i = 0; i < course.length; i++) {
-        txtValue = course[i].textContent || course[i].innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            course[i].style.display = "";
-        } else {
-            course[i].style.display = "none";
-        }
-    }
+const courses = [
+    {name: "Fireweed Meadows Golf Course", location: "Anchor Point, AK", lat: 59.773222, lon: -151.820102},
+    {name: "Anchorage Golf Course", location: "Anchorage, AK", lat: 61.127089, lon: -149.814495},
+    {name: "Russian Jack Springs Golf Course", location: "Anchorage, AK", lat: 61.206435, lon: -149.788303},
+    {name: "Tanglewood Lakes Golf Club", location: "Anchorage, AK", lat: 61.113415, lon: -149.853757},
+    {name: "Eagleglen Golf Course", location: "Elmendorf Afb, AK", lat: 61.233041, lon: -149.824373},
+    {name: "Fairbanks Golf and Country Club", location: "Fairbanks, AK", lat: 64.877632, lon: -147.816536}
+    // Add more courses as needed
+];
+
+window.onload = function() {
+    displayCourses(courses);
+};
+
+function displayCourses(courseArray) {
+    const courseList = document.getElementById("courseList");
+    courseList.innerHTML = '';
+    courseArray.forEach(course => {
+        const courseDiv = document.createElement('div');
+        courseDiv.className = 'course-item';
+        courseDiv.textContent = `${course.name} - ${course.location}`;
+        courseList.appendChild(courseDiv);
+    });
 }
+
+function filterCourses() {
+    const input = document.getElementById("courseSearchInput");
+    const filter = input.value.toUpperCase();
+    const filteredCourses = courses.filter(course => course.name.toUpperCase().includes(filter));
+    displayCourses(filteredCourses);
+}
+
